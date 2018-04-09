94f634e962f593ecb09e07b8285b461606621179
Jay Bednar <ironviralmedia@gmail.com>
2018-04-09 18:54:51 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,6 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    # binding.pry
     @pet = Pet.create(params[:pet])
     if params[:pet][:owner_id]
       @pet.owner = Owner.find(params[:pet][:owner_id])
@@ -35,7 +34,6 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do
-    # binding.pry
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
     if params[:owner][:name] != ""
