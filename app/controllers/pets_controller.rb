a013a29529feeeff6b41728d88d1b5ec49274932
Jay Bednar <ironviralmedia@gmail.com>
2018-04-09 18:44:28 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -39,9 +39,10 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
     if !!params[:pet][:owner_id]
-      @pet.owner = Owner.find(params[:pet][:owner_id])
-    elsif !!params[:owner][:name]
       @pet.owner = Owner.create(params[:owner])
+    #   @pet.owner = Owner.find(params[:pet][:owner_id])
+    # elsif !!params[:owner][:name]
+    #   @pet.owner = Owner.create(params[:owner])
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
